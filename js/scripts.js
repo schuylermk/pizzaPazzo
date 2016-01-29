5294f62adf3a798daf6c562d5649130352a6fef9
schuylermk <schuyler.m.k@gmail.com>
2016-01-29 11:54:12 -0800

    add spec and code for size upcharge

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,39 @@
-function Pizza(type, size) {
+function Pizza(type, size, extra) {
   this.type = type;
   this.size = size;
+  this.extra = extra;
 }
 Pizza.prototype.price = function() {
   var pizzaPrice = 5;
+// Types of pizze
+  if(this.type === "margherita") {
+    pizzaPrice += 1;
+  } else if (this.type === "margherita con bufala") {
+    pizzaPrice += 2;
+  } else if (this.type === "capricciosa") {
+    pizzaPrice += 2;
+  } else if (this.type === "tonnata") {
+    pizzaPrice += 2;
+  } else if (this.type === "wurstel e patatine") {
+    pizzaPrice += 3;
+  } else if (this.type === "salsiccia e friarielli") {
+    pizzaPrice += 3;
+  } else if (this.type === "pizza al 4 stagioni") {
+    pizzaPrice += 4;
+  } else {}
+// Size Upcharges
+  if (this.size === "medium") {
+    pizzaPrice += 2;
+  } else if (this.size === "large") {
+    pizzaPrice += 4;
+  } else {}
+// Extras
+  if ((this.extra === "rucola") || (this.extra === "grana")) {
+    pizzaPrice += 1;
+  } else if ((this.extra === "acchiughe") || (this.extra === "olio di peperoncini diavoletti")) {
+    pizzaPrice += 2;
+  } else if (this.extra === "due gramme di tartufo bianco") {
+    pizzaPrice += 15;
+  } else {}
   return "$" + pizzaPrice.toFixed(2);
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,31 @@
 describe("Pizza", function() {
   it("creates an object pizza and stores its properties", function() {
-    var hotPizza = new Pizza("marinara", "small");
+    var hotPizza = new Pizza("marinara", "small", "grana");
     expect(hotPizza.type).to.equal("marinara");
     expect(hotPizza.size).to.equal("small");
+    expect(hotPizza.extra).to.equal("grana");
   });
+// Types of pizzas
   it("returns the pizza price", function() {
-    var hotPizza = new Pizza("marinara", "small");
+    var hotPizza = new Pizza("marinara", "small", "");
     expect(hotPizza.price()).to.equal("$5.00");
+  });
+  it("returns the price for types of pizza other than plain/marinara", function() {
+    var hotPizza = new Pizza("margherita", "small", "");
+    expect(hotPizza.price()).to.equal("$6.00");
+  });
+// Size Modifications
+  it("increases price $2 for medium", function() {
+    var hotPizza = new Pizza("marinara", "medium", "");
+    expect(hotPizza.price()).to.equal("$7.00");
+  });
+  it("increases price $4 for large", function() {
+    var hotPizza = new Pizza("marinara", "large", "");
+    expect(hotPizza.price()).to.equal("$9.00");
+  });
+// Additions
+  it("increases a set amount for extras to price of a pizza", function() {
+    var hotPizza = new Pizza("marinara", "small", "due gramme di tartufo bianco");
+    expect(hotPizza.price()).to.equal("$20.00");
   });
 });
