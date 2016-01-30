0da4fe0f862ba7c0bc948885c5fdc6ca817e9540
schuylermk <schuyler.m.k@gmail.com>
2016-01-29 17:11:27 -0800

    Attaches jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@ function Pizza(type, size, extra) {
   this.size = size;
   this.extra = extra;
 }
+
 Pizza.prototype.price = function() {
   var pizzaPrice = 5;
+
 // Types of pizze
   if(this.type === "margherita") {
     pizzaPrice += 1;
@@ -35,3 +37,22 @@ Pizza.prototype.price = function() {
   } else { }
   return "$" + pizzaPrice.toFixed(2);
 }
+
+$(document).ready(function() {
+  $("form.pizza-pencil").submit(function(event) {
+    event.preventDefault();
+    var inputType = $("select#type").val();
+    var inputSize = $("select#size").val();
+    // $("form#more").submit(function(event) {
+    //   var inputExtra = ($("input#extra").val());
+
+      var hotPizza = new Pizza(inputType, inputSize, inputExtra);
+
+      $(".pizza-price").text(hotPizza.price());
+      $(".pizza-type").text(hotPizza.type);
+      $(".pizza-size").text(hotPizza.size);
+      // $(".pizza-extra")
+      $("#results").show();
+    // });
+  });
+});
