c4b23baa58fa9a5dc069a5e9d4081e6d8a6ff148
schuylermk <schuyler.m.k@gmail.com>
2016-02-04 14:49:32 -0800

    Adds a little styling

--- js/scripts.js
+++ js/scripts.js
@@ -37,17 +37,13 @@ function Pizza(type, size, extra) {
     this.extra.forEach(function(extraTopping) {
       if (extraTopping === "arugala") {
         extraCost += 1;
-      }
-      if (extraTopping === "grana padano") {
+      } if (extraTopping === "grana padano") {
         extraCost += 1;
-      }
-      if (extraTopping === "anchovies") {
+      } if (extraTopping === "anchovies") {
         extraCost += 2;
-      }
-      if (extraTopping === "olio di peperoncini") {
+      } if (extraTopping === "olio di peperoncini") {
         extraCost += 2;
-      }
-      if (extraTopping === "tartufi"){
+      } if (extraTopping === "tartufi"){
         extraCost += 15;
       }
     });
@@ -57,35 +53,25 @@ function Pizza(type, size, extra) {
   Pizza.prototype.pizzaPrice = function() {
    return this.typePrice() + this.sizePrice() + this.extraPrice();
   };
-
-
 $(document).ready(function() {
   var wholePrice = 0;
   $("form#pizza-pencil").submit(function(event) {
     event.preventDefault();
-
     var inputType = $("select#type").val();
     var inputSize = $("select#size").val();
-
-    // You have multiple inputs, so $("input:checkbox[name=extra]:checked") will return an array
-    // Try running a .forEach(function(extra) { push to array })
-    // var inputExtra = $("input:checkbox[name=extra]:checked").val();
     var inputExtra = [];
     $("input:checkbox[name='extra']:checked").each(function() {
       inputExtra.push($(this).val());
     });
-
     var newPizza = new Pizza(inputType, inputSize, inputExtra);
     var newPrice = newPizza.pizzaPrice();
     wholePrice += newPrice;
 
     $("div#results").show();
     $("div#selections").append('<li>' + 'We\'ll start making your' + ' ' + newPizza.size + ' ' + newPizza.type + ' right away!' + '</li>');
-
     $("div#wholePrice").empty().append('<li>' + 'Your new total is going to be $' + wholePrice + '.     Grazie mille!' + '</li>');
+    if (inputExtra > 0) {
+      $("div#wholePrice").empty().append('<li>' + '...and don\'t worry, we didn\'t forget the ' + inputExtra + '!' + '</li>');
+    }
   });
 });
-
-// // if (extra > 0) {
-// //   append('<li>' + '...and don\'t worry, we didn\'t forget the ' +  )
-// // }
